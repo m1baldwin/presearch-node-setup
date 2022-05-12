39d1aa36d9ee15acb19a56c5a21013d23ec65862
m1baldwin <michael.baldwin101@gmail.com>
2022-05-12 15:12:38 +0000

    second initial commit

---
+++ presearch-node-start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker stop presearch-node ; docker rm presearch-node ; docker stop presearch-auto-updater ; docker rm presearch-auto-updater ; docker run -d --name presearch-auto-updater --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock presearch/auto-updater --cleanup --interval 900 presearch-auto-updater presearch-node ; docker pull presearch/node ; docker run -dt --name presearch-node --restart=unless-stopped -v presearch-node-storage:/app/node -e REGISTRATION_CODE=$1 presearch/node ; docker logs -f presearch-node
---
+++ setup-presearch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Based on the instructions here: https://docs.docker.com/engine/install/ubuntu/
+#and here: https://docs.docker.com/engine/install/linux-postinstall/
+
+echo "Welcome! Let's setup your node!"
+
+if [ -z $1 ]
+then
+	echo "You need to provide the presearch registration key"
+	exit 1
+fi
+
+PRESEARCH_REGISTRATION_CODE=$PRESEARCH_REGISTRATION_CODE
+
+echo "You provided the following PRESEARCH_REGISTRATION_CODE=$PRESEARCH_REGISTRATION_CODE
+
+#Update apt package and install packages to allow apt t o use repo over https
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+#add docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+#setup the stable repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Update Apt and install dcoker engine, containerd, and docker compose
+
+
+#Hello-world
+sudo docker run hello-world
+
+#add user to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+#update group settings without needign to log out
+newgrp docker 
+
+#setup and run presearch
+./presearch-node-start.sh $PRESEARCH_REGISTRATION_CODE
